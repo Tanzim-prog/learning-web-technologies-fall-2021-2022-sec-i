d02b73c91ac1ee5d5992c79b42d864747bc3b7e4
unknown <you@example.com>
2021-11-24 20:56:47 +0600

    Final Commit

--- FINAL_LAB_PERFORMANCE_2/Ajax.php
+++ FINAL_LAB_PERFORMANCE_2/Ajax.php
@@ -4,6 +4,20 @@ if(isset($_POST))
     extract($_POST);
     $operators = array('+', '-', '*', '/', '%', '+/-','.', '=');
 
-
+    case 'equal':
+            $last = '';
+            if(strlen($old)>0)
+            {
+                $last = substr($old, -1);
+            }
+            if($old <> '' and !in_array($last, $operators))
+            {
+               $out = eval("return ($old);");
+            }
+            else
+            {
+                $out = $old;
+            }
+        break;
 
 ?>
